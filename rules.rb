ae540425c6ecc9468dcb4000c745794518db5b41
Chris Montgomery <chris@montchr.io>
2016-12-14 23:19:12 -0500

    simplify directory tagging

--- rules.rb
+++ rules.rb
@@ -7,47 +7,26 @@ class Sampler
 
     @dir_root = '/Users/montchr/Music/0-sounds-0'
     @dir_in = @dir_root + '/00000 in'
-    @dir_samples = @dir_root + '/00001 library copy/00002 samples'
-    @tags = ['s']
+    @dir_samples = @dir_root + '/00001 library/00002 samples'
   end
 
   attr_reader :dir_root, :dir_in, :dir_samples
 
-  def get_subdirs(root)
-    Dir.entries(root).select do |subdir|
-      path = File.join(root, subdir)
-      File.directory?(path) && !(subdir == '.' || subdir == '..')
-    end
-  end
-
-  def prepare_tags(tags)
+  # Sanitize tags
+  def sanitize_tags(tags)
     tags.map! do |tag|
       tag.delete!('-')
       tag.tr!('+', 'n')
       's.' + tag
     end
-    tags + @tags
   end
 
-  def add_tags(dir_path, allowed_names, subdirs = false)
-    if subdirs
-      get_subdirs(dir_path).each do |subdir_name|
-        subdir_path = dir_path + '/' + subdir_name
-        tag_dirname(subdir_path, allowed_names)
-      end
-    else
-      tag_dirname(dir_path, allowed_names)
-    end
-  end
-
-  private
-
-  def tag_dirname(dir_path, allowed_names)
-    dir_name = File.basename(dir_path)
+  # Add tags to files within `dir_path` based on itself
+  def tag_dirname(dir_path)
+    dir_tag = sanitize_tags([dir_path.tr('/', '.')])
+    dir_path = @dir_samples + '/' + dir_path
     @maid.dir(dir_path + '/**/*.wav').each do |file|
-      next unless allowed_names.include? dir_name
-      tags = prepare_tags([dir_name])
-      @maid.add_tag(file, tags)
+      @maid.add_tag(file, dir_tag)
     end
   end
 end
@@ -56,13 +35,24 @@ Maid.rules do
   rule 'Sampler: tag based on directory names' do
     s = Sampler.new
 
-    dir_root_allowed = %w(field radio session)
-    s.add_tags(s.dir_samples, dir_root_allowed, subdirs: true)
+    # Add basic `s` tag to every wav
+    dir(s.dir_samples + '/**/*.wav').each do |file|
+      add_tag(file, 's')
+    end
+
+    # Tag basic types
+    s.tag_dirname 'field'
+    s.tag_dirname 'session'
+
+    # Tag source types
+    s.tag_dirname 'src'
+    %w(intv movies tv xxx radio radio/shortwave).each do |type_dir|
+      s.tag_dirname 'src/' + type_dir
+    end
 
     # Tag music genres
-    dir_music_allowed_names = ['classical', 'electronic', 'hip-hop',
-                               'jazz', 'r+b', 'rock']
-    dir_music = s.dir_samples + '/src/music'
-    s.add_tags(dir_music, dir_music_allowed_names, subdirs: true)
+    %w(classical electronic hiphop jazz rb rock).each do |genre_dir|
+      s.tag_dirname 'src/music/' + genre_dir
+    end
   end
 end
